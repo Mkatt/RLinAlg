a4cdd6325277f2b9dea45878dea507de29d39ca6
Muhammad Kattom <125837246+Mkatt@users.noreply.github.com>
2023-11-11 12:32:12 -0600

    Add files via upload

---
+++ src/lib.rs
@@ -0,0 +1,178 @@
+use rayon::prelude::*;
+use ndarray::{Array2, Array1, Zip, arr2, ArrayView1};
+
+pub struct Matrix {
+    data: Array2<f64>,
+}
+impl Matrix {
+    pub fn add(&self, other: &Matrix) -> Matrix {
+        if self.data.dim() != other.data.dim() {
+            panic!("Matrices must be of the same dimensions");
+        }
+
+        let sum_data = &self.data + &other.data;
+        Matrix { data: sum_data }
+    }
+
+    pub fn multiply(&self, other: &Matrix) -> Matrix {
+        if self.data.ncols() != other.data.nrows() {
+            panic!("Inner matrix dimensions must match for multiplication");
+        }
+
+        let n = self.data.nrows();
+        let m = other.data.ncols();
+        let p = self.data.ncols();
+
+
+        let mut partial_results: Vec<Array2<f64>> = (0..n).into_par_iter()
+            .map(|i| {
+                let mut row_result = Array2::<f64>::zeros((1, m));
+                for j in 0..m {
+                    row_result[[0, j]] = (0..p).map(|k| self.data[[i, k]] * other.data[[k, j]]).sum();
+                }
+                row_result
+            })
+            .collect();
+
+        let mut result = Array2::<f64>::zeros((n, m));
+        for (i, partial) in partial_results.into_iter().enumerate() {
+            result.row_mut(i).assign(&partial.row(0));
+        }
+
+        Matrix { data: result }
+    }
+}
+
+
+pub struct Vector {
+    data: Array1<f64>,
+}
+impl Vector {
+    pub fn add(&self, other: &Vector) -> Vector {
+        let self_slice = self.data.view();
+        let other_slice = other.data.view();
+
+        let sum_data = self_slice.as_slice().unwrap().par_iter()
+                                .zip(other_slice.as_slice().unwrap().par_iter())
+                                .map(|(&a, &b)| a + b)
+                                .collect::<Vec<f64>>(); 
+
+        Vector { data: Array1::from(sum_data) }
+    }
+    pub fn dot(&self, other: &Vector) -> f64 {
+        if self.data.len() != other.data.len() {
+            panic!("Vectors must be of the same length");
+        }
+
+        let self_slice = self.data.as_slice().unwrap();
+        let other_slice = other.data.as_slice().unwrap();
+
+        self_slice.par_iter()
+                  .zip(other_slice.par_iter())
+                  .map(|(&a, &b)| a * b)
+                  .sum()
+    }
+    pub fn magnitude(&self) -> f64 {
+        self.data.iter()
+                 .map(|&x| x * x)
+                 .sum::<f64>()
+                 .sqrt()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::f64::consts::SQRT_2;
+
+    #[test]
+    fn test_matrix_addition() {
+        let a = Matrix { data: Array2::from_shape_vec((2, 2), vec![1.0, 2.0, 3.0, 4.0]).unwrap() };
+        let b = Matrix { data: Array2::from_shape_vec((2, 2), vec![4.0, 3.0, 2.0, 1.0]).unwrap() };
+        let result = a.add(&b);
+
+        let expected = Array2::from_shape_vec((2, 2), vec![5.0, 5.0, 5.0, 5.0]).unwrap();
+        assert_eq!(result.data, expected);
+    }
+
+    #[test]
+    #[should_panic(expected = "Matrices must be of the same dimensions")]
+    fn test_matrix_addition_panic() {
+        let a = Matrix { data: Array2::from_shape_vec((2, 2), vec![1.0, 2.0, 3.0, 4.0]).unwrap() };
+        let b = Matrix { data: Array2::from_shape_vec((3, 2), vec![4.0, 3.0, 2.0, 1.0, 0.0, -1.0]).unwrap() };
+        a.add(&b);
+    }
+
+    #[test]
+    fn test_matrix_multiplication() {
+        let a = Matrix { data: Array2::from_shape_vec((2, 2), vec![1.0, 2.0, 3.0, 4.0]).unwrap() };
+        let b = Matrix { data: Array2::from_shape_vec((2, 2), vec![2.0, 0.0, 1.0, 2.0]).unwrap() };
+        let result = a.multiply(&b);
+
+        let expected = Array2::from_shape_vec((2, 2), vec![4.0, 4.0, 10.0, 8.0]).unwrap();
+        assert_eq!(result.data, expected);
+    }
+
+    #[test]
+    #[should_panic(expected = "Inner matrix dimensions must match for multiplication")]
+    fn test_matrix_multiplication_panic() {
+        let a = Matrix { data: Array2::from_shape_vec((2, 3), vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0]).unwrap() };
+        let b = Matrix { data: Array2::from_shape_vec((2, 2), vec![7.0, 8.0, 9.0, 10.0]).unwrap() };
+        a.multiply(&b);
+    }
+
+    #[test]
+    fn test_vector_addition() {
+        let a = Vector { data: Array1::from_vec(vec![1.0, 2.0, 3.0]) };
+        let b = Vector { data: Array1::from_vec(vec![4.0, 5.0, 6.0]) };
+        let result = a.add(&b);
+
+        let expected = Array1::from_vec(vec![5.0, 7.0, 9.0]);
+        assert_eq!(result.data, expected);
+    }
+
+    #[test]
+    fn test_vector_dot_product() {
+        let a = Vector { data: Array1::from_vec(vec![1.0, 2.0, 3.0]) };
+        let b = Vector { data: Array1::from_vec(vec![4.0, 5.0, 6.0]) };
+        let result = a.dot(&b);
+
+        let expected = 32.0; // 1*4 + 2*5 + 3*6
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    #[should_panic(expected = "Vectors must be of the same length")]
+    fn test_vector_dot_product_panic() {
+        let a = Vector { data: Array1::from_vec(vec![1.0, 2.0, 3.0]) };
+        let b = Vector { data: Array1::from_vec(vec![4.0, 5.0]) };
+        a.dot(&b);
+    }
+    #[test]
+    fn test_vector_magnitude_zero() {
+        let v = Vector { data: Array1::from_vec(vec![0.0, 0.0, 0.0]) };
+        let mag = v.magnitude();
+        assert_eq!(mag, 0.0);
+    }
+
+    #[test]
+    fn test_vector_magnitude_basic() {
+        let v = Vector { data: Array1::from_vec(vec![3.0, 4.0]) };
+        let mag = v.magnitude();
+        assert_eq!(mag, 5.0); // 3-4-5 right triangle
+    }
+
+    #[test]
+    fn test_vector_magnitude_negative_values() {
+        let v = Vector { data: Array1::from_vec(vec![-3.0, -4.0]) };
+        let mag = v.magnitude();
+        assert_eq!(mag, 5.0); // Magnitude should be positive
+    }
+
+    #[test]
+    fn test_vector_magnitude_fractional() {
+        let v = Vector { data: Array1::from_vec(vec![1.0 / SQRT_2, 1.0 / SQRT_2]) };
+        let mag = v.magnitude();
+        assert!((mag - 1.0).abs() < 1e-10); // Should be close to 1 with a small error margin
+    }
+}
