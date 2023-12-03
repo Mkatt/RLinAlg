c19ed64f77736ffe14721eaa9f9d00e007c89f5f
caupcakes <78387392+caupcakes@users.noreply.github.com>
2023-12-03 00:43:14 -0600

    add transpose determinant, and normalize

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use rayon::prelude::*;
 use ndarray::{Array2, Array1, Zip, arr2, ArrayView1};
+use nalgebra::{Matrix as NaMatrix, Vector as NaVector, Dim};
+
 
 pub struct Matrix {
     data: Array2<f64>,
@@ -41,9 +43,67 @@ impl Matrix {
 
         Matrix { data: result }
     }
+
+    pub fn transpose(&self) -> Matrix {
+        Matrix {
+            data: self.data.t().to_owned(),
+        }
+    }
+
+    pub fn determinant(&self) -> Option<f64> {
+        let (rows, cols) = self.data.dim();
+        if rows != cols {
+            return None; // Not a square matrix
+        }
+
+        Some(self.calculate_determinant(&self.data))
+    }
+
+    pub(self) fn calculate_determinant(&self, matrix: &Array2<f64>) -> f64 {
+        let (rows, _) = matrix.dim();
+
+        if rows == 1 {
+            return matrix[[0, 0]];
+        }
+
+        let mut determinant = 0.0;
+        let mut sign = 1.0;
+
+        for col in 0..rows {
+            let minor = self.create_minor(matrix, 0, col);
+            determinant += sign * matrix[[0, col]] * self.calculate_determinant(&minor);
+            sign *= -1.0;
+        }
+
+        determinant
+    }
+
+    pub(self) fn create_minor(&self, matrix: &Array2<f64>, row_to_exclude: usize, col_to_exclude: usize) -> Array2<f64> {
+        let (rows, cols) = matrix.dim();
+        let mut minor = Array2::<f64>::zeros((rows - 1, cols - 1));
+
+        let mut minor_row = 0;
+        let mut minor_col;
+
+        for row in 0..rows {
+            if row == row_to_exclude {
+                continue;
+            }
+            minor_col = 0;
+            for col in 0..cols {
+                if col != col_to_exclude {
+                    minor[[minor_row, minor_col]] = matrix[[row, col]];
+                    minor_col += 1;
+                }
+            }
+            minor_row += 1;
+        }
+
+        minor
+    }
 }
 
-
+#[derive(Clone)]
 pub struct Vector {
     data: Array1<f64>,
 }
@@ -77,6 +137,23 @@ impl Vector {
                  .map(|&x| x * x)
                  .sum::<f64>()
                  .sqrt()
+    }
+
+    pub fn normalize(&self) -> Vector {
+        let mag = self.magnitude();
+        if mag == 0.0 {
+            return self.clone();
+        }
+
+        let normalized_data: Vec<f64> = self
+            .data
+            .iter()
+            .map(|&x| x / mag)
+            .collect();
+
+        Vector {
+            data: Array1::from(normalized_data),
+        }
     }
 }
 
@@ -174,5 +251,48 @@ mod tests {
         let v = Vector { data: Array1::from_vec(vec![1.0 / SQRT_2, 1.0 / SQRT_2]) };
         let mag = v.magnitude();
         assert!((mag - 1.0).abs() < 1e-10); // Should be close to 1 with a small error margin
+    }
+
+    #[test]
+    fn test_vector_normalization() {
+        let v = Vector {
+            data: Array1::from_vec(vec![3.0, 4.0]),
+        };
+        let normalized = v.normalize();
+
+        let expected = Array1::from_vec(vec![0.6, 0.8]);
+        assert_eq!(normalized.data, expected);
+    }
+
+    #[test]
+    fn test_matrix_transpose() {
+        let a = Matrix {
+            data: Array2::from_shape_vec((2, 3), vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0]).unwrap(),
+        };
+        let transposed = a.transpose();
+
+        let expected = Array2::from_shape_vec((3, 2), vec![1.0, 4.0, 2.0, 5.0, 3.0, 6.0]).unwrap();
+        assert_eq!(transposed.data, expected);
+    }
+
+    #[test]
+    fn test_matrix_determinant() {
+        let a = Matrix {
+            data: Array2::from_shape_vec((2, 2), vec![1.0, 2.0, 3.0, 4.0]).unwrap(),
+        };
+        let determinant = a.determinant().unwrap();
+
+        let expected = -2.0;
+        assert_eq!(determinant, expected);
+    }
+
+    #[test]
+    fn test_matrix_determinant_non_square() {
+        let a = Matrix {
+            data: Array2::from_shape_vec((2, 3), vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0]).unwrap(),
+        };
+        let determinant = a.determinant();
+
+        assert_eq!(determinant, None);
     }
 }
