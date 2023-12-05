32258e7d027b3edc2db76a9cf523df7b16b05113
Muhammad Kattom <muhammadkattom@gmail.com>
2023-12-04 23:32:45 -0600

    C2

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,35 @@
+use ndarray::{Array1, Array2};
 use rayon::prelude::*;
-use ndarray::{Array2, Array1, Zip, arr2, ArrayView1};
-use nalgebra::{Matrix as NaMatrix, Vector as NaVector, Dim};
-
 
 pub struct Matrix {
-    data: Array2<f64>,
+    pub data: Array2<f64>,
 }
 impl Matrix {
-    pub fn add(&self, other: &Matrix) -> Matrix {
+    pub fn add(&self, other: &Matrix) -> Result<Matrix, String> {
         if self.data.dim() != other.data.dim() {
-            panic!("Matrices must be of the same dimensions");
+            return Err("Matrices must be of the same dimensions".to_string());
         }
 
         let sum_data = &self.data + &other.data;
-        Matrix { data: sum_data }
+        Ok(Matrix { data: sum_data })
     }
 
-    pub fn multiply(&self, other: &Matrix) -> Matrix {
+    pub fn multiply(&self, other: &Matrix) -> Result<Matrix, String> {
         if self.data.ncols() != other.data.nrows() {
-            panic!("Inner matrix dimensions must match for multiplication");
+            return Err("Inner matrix dimensions must match for multiplication".to_string());
         }
 
         let n = self.data.nrows();
         let m = other.data.ncols();
         let p = self.data.ncols();
 
-
-        let mut partial_results: Vec<Array2<f64>> = (0..n).into_par_iter()
+        let partial_results: Vec<Array2<f64>> = (0..n)
+            .into_par_iter()
             .map(|i| {
                 let mut row_result = Array2::<f64>::zeros((1, m));
                 for j in 0..m {
-                    row_result[[0, j]] = (0..p).map(|k| self.data[[i, k]] * other.data[[k, j]]).sum();
+                    row_result[[0, j]] =
+                        (0..p).map(|k| self.data[[i, k]] * other.data[[k, j]]).sum();
                 }
                 row_result
             })
@@ -41,7 +40,7 @@ impl Matrix {
             result.row_mut(i).assign(&partial.row(0));
         }
 
-        Matrix { data: result }
+        Ok(Matrix { data: result })
     }
 
     pub fn transpose(&self) -> Matrix {
@@ -78,7 +77,12 @@ impl Matrix {
         determinant
     }
 
-    pub(self) fn create_minor(&self, matrix: &Array2<f64>, row_to_exclude: usize, col_to_exclude: usize) -> Array2<f64> {
+    pub(self) fn create_minor(
+        &self,
+        matrix: &Array2<f64>,
+        row_to_exclude: usize,
+        col_to_exclude: usize,
+    ) -> Array2<f64> {
         let (rows, cols) = matrix.dim();
         let mut minor = Array2::<f64>::zeros((rows - 1, cols - 1));
 
@@ -101,42 +105,144 @@ impl Matrix {
 
         minor
     }
+
+    pub fn identity(size: usize) -> Matrix {
+        let mut data = Array2::<f64>::zeros((size, size));
+        for i in 0..size {
+            data[[i, i]] = 1.0;
+        }
+        Matrix { data }
+    }
+
+    pub fn zero(rows: usize, cols: usize) -> Matrix {
+        let data = Array2::<f64>::zeros((rows, cols));
+        Matrix { data }
+    }
+
+    pub fn inverse(&self) -> Result<Matrix, String> {
+        let (rows, cols) = self.data.dim();
+
+        if rows != cols {
+            return Err("Only square matrices can be inverted".to_string());
+        }
+
+        let det = self.determinant().unwrap_or(0.0);
+
+        if det == 0.0 {
+            return Err("Matrix is not invertible".to_string());
+        }
+
+        let cofactors: Vec<_> = (0..rows)
+            .into_par_iter()
+            .flat_map(|i| {
+                (0..cols).into_par_iter().map(move |j| {
+                    let minor = self.create_minor(&self.data, i, j);
+                    let cofactor =
+                        self.calculate_determinant(&minor) * (-1.0f64).powi((i + j) as i32);
+                    (i, j, cofactor)
+                })
+            })
+            .collect();
+
+        let mut adjugate = Array2::<f64>::zeros((rows, cols));
+        for (i, j, cofactor) in cofactors {
+            adjugate[[j, i]] = cofactor;
+        }
+
+        let inverse_data = adjugate.mapv(|x| x / det);
+
+        Ok(Matrix { data: inverse_data })
+    }
+
+    pub fn lu_decomposition(&self) -> Result<(Matrix, Matrix), String> {
+        let n = self.data.nrows();
+        if n != self.data.ncols() {
+            return Err("Matrix must be square".to_string());
+        }
+
+        let mut lower = Array2::<f64>::zeros((n, n));
+        let mut upper = Array2::<f64>::zeros((n, n));
+
+        for i in 0..n {
+            for k in i..n {
+                let sum = (0..i).fold(0.0, |sum, j| sum + lower[[i, j]] * upper[[j, k]]);
+                upper[[i, k]] = self.data[[i, k]] - sum;
+            }
+
+            for k in i..n {
+                if i == k {
+                    lower[[i, i]] = 1.0;
+                } else {
+                    let sum = (0..i).fold(0.0, |sum, j| sum + lower[[k, j]] * upper[[j, i]]);
+                    lower[[k, i]] = (self.data[[k, i]] - sum) / upper[[i, i]];
+                }
+            }
+        }
+
+        Ok((Matrix { data: lower }, Matrix { data: upper }))
+    }
+
+    pub fn l1_norm(&self) -> f64 {
+        self.data.iter().map(|&x| x.abs()).sum()
+    }
+
+    pub fn l2_norm(&self) -> f64 {
+        self.data.iter().map(|&x| x * x).sum::<f64>().sqrt()
+    }
+
+    pub fn infinity_norm(&self) -> f64 {
+        self.data
+            .axis_iter(ndarray::Axis(0))
+            .map(|row| row.iter().map(|&x| x.abs()).sum::<f64>())
+            .max_by(|x, y| x.partial_cmp(y).unwrap())
+            .unwrap_or(0.0)
+    }
+
+    pub fn trace(&self) -> f64 {
+        self.data.diag().iter().sum()
+    }
 }
 
 #[derive(Clone)]
 pub struct Vector {
-    data: Array1<f64>,
+    pub data: Array1<f64>,
 }
 impl Vector {
     pub fn add(&self, other: &Vector) -> Vector {
         let self_slice = self.data.view();
         let other_slice = other.data.view();
 
-        let sum_data = self_slice.as_slice().unwrap().par_iter()
-                                .zip(other_slice.as_slice().unwrap().par_iter())
-                                .map(|(&a, &b)| a + b)
-                                .collect::<Vec<f64>>(); 
+        let sum_data = self_slice
+            .as_slice()
+            .unwrap()
+            .par_iter()
+            .zip(other_slice.as_slice().unwrap().par_iter())
+            .map(|(&a, &b)| a + b)
+            .collect::<Vec<f64>>();
 
-        Vector { data: Array1::from(sum_data) }
+        Vector {
+            data: Array1::from(sum_data),
+        }
     }
-    pub fn dot(&self, other: &Vector) -> f64 {
+    pub fn dot(&self, other: &Vector) -> Result<f64, String> {
         if self.data.len() != other.data.len() {
-            panic!("Vectors must be of the same length");
+            return Err("Vectors must be of the same length".to_string());
         }
 
         let self_slice = self.data.as_slice().unwrap();
         let other_slice = other.data.as_slice().unwrap();
 
-        self_slice.par_iter()
-                  .zip(other_slice.par_iter())
-                  .map(|(&a, &b)| a * b)
-                  .sum()
+        let dot_product = self_slice
+            .par_iter()
+            .zip(other_slice.par_iter())
+            .map(|(&a, &b)| a * b)
+            .sum();
+
+        Ok(dot_product)
     }
+
     pub fn magnitude(&self) -> f64 {
-        self.data.iter()
-                 .map(|&x| x * x)
-                 .sum::<f64>()
-                 .sqrt()
+        self.data.iter().map(|&x| x * x).sum::<f64>().sqrt()
     }
 
     pub fn normalize(&self) -> Vector {
@@ -145,154 +251,18 @@ impl Vector {
             return self.clone();
         }
 
-        let normalized_data: Vec<f64> = self
-            .data
-            .iter()
-            .map(|&x| x / mag)
-            .collect();
+        let normalized_data: Vec<f64> = self.data.iter().map(|&x| x / mag).collect();
 
         Vector {
             data: Array1::from(normalized_data),
         }
     }
-}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::f64::consts::SQRT_2;
-
-    #[test]
-    fn test_matrix_addition() {
-        let a = Matrix { data: Array2::from_shape_vec((2, 2), vec![1.0, 2.0, 3.0, 4.0]).unwrap() };
-        let b = Matrix { data: Array2::from_shape_vec((2, 2), vec![4.0, 3.0, 2.0, 1.0]).unwrap() };
-        let result = a.add(&b);
-
-        let expected = Array2::from_shape_vec((2, 2), vec![5.0, 5.0, 5.0, 5.0]).unwrap();
-        assert_eq!(result.data, expected);
+    pub fn l1_norm(&self) -> f64 {
+        self.data.par_iter().map(|&x| x.abs()).sum()
     }
 
-    #[test]
-    #[should_panic(expected = "Matrices must be of the same dimensions")]
-    fn test_matrix_addition_panic() {
-        let a = Matrix { data: Array2::from_shape_vec((2, 2), vec![1.0, 2.0, 3.0, 4.0]).unwrap() };
-        let b = Matrix { data: Array2::from_shape_vec((3, 2), vec![4.0, 3.0, 2.0, 1.0, 0.0, -1.0]).unwrap() };
-        a.add(&b);
-    }
-
-    #[test]
-    fn test_matrix_multiplication() {
-        let a = Matrix { data: Array2::from_shape_vec((2, 2), vec![1.0, 2.0, 3.0, 4.0]).unwrap() };
-        let b = Matrix { data: Array2::from_shape_vec((2, 2), vec![2.0, 0.0, 1.0, 2.0]).unwrap() };
-        let result = a.multiply(&b);
-
-        let expected = Array2::from_shape_vec((2, 2), vec![4.0, 4.0, 10.0, 8.0]).unwrap();
-        assert_eq!(result.data, expected);
-    }
-
-    #[test]
-    #[should_panic(expected = "Inner matrix dimensions must match for multiplication")]
-    fn test_matrix_multiplication_panic() {
-        let a = Matrix { data: Array2::from_shape_vec((2, 3), vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0]).unwrap() };
-        let b = Matrix { data: Array2::from_shape_vec((2, 2), vec![7.0, 8.0, 9.0, 10.0]).unwrap() };
-        a.multiply(&b);
-    }
-
-    #[test]
-    fn test_vector_addition() {
-        let a = Vector { data: Array1::from_vec(vec![1.0, 2.0, 3.0]) };
-        let b = Vector { data: Array1::from_vec(vec![4.0, 5.0, 6.0]) };
-        let result = a.add(&b);
-
-        let expected = Array1::from_vec(vec![5.0, 7.0, 9.0]);
-        assert_eq!(result.data, expected);
-    }
-
-    #[test]
-    fn test_vector_dot_product() {
-        let a = Vector { data: Array1::from_vec(vec![1.0, 2.0, 3.0]) };
-        let b = Vector { data: Array1::from_vec(vec![4.0, 5.0, 6.0]) };
-        let result = a.dot(&b);
-
-        let expected = 32.0; // 1*4 + 2*5 + 3*6
-        assert_eq!(result, expected);
-    }
-
-    #[test]
-    #[should_panic(expected = "Vectors must be of the same length")]
-    fn test_vector_dot_product_panic() {
-        let a = Vector { data: Array1::from_vec(vec![1.0, 2.0, 3.0]) };
-        let b = Vector { data: Array1::from_vec(vec![4.0, 5.0]) };
-        a.dot(&b);
-    }
-    #[test]
-    fn test_vector_magnitude_zero() {
-        let v = Vector { data: Array1::from_vec(vec![0.0, 0.0, 0.0]) };
-        let mag = v.magnitude();
-        assert_eq!(mag, 0.0);
-    }
-
-    #[test]
-    fn test_vector_magnitude_basic() {
-        let v = Vector { data: Array1::from_vec(vec![3.0, 4.0]) };
-        let mag = v.magnitude();
-        assert_eq!(mag, 5.0); // 3-4-5 right triangle
-    }
-
-    #[test]
-    fn test_vector_magnitude_negative_values() {
-        let v = Vector { data: Array1::from_vec(vec![-3.0, -4.0]) };
-        let mag = v.magnitude();
-        assert_eq!(mag, 5.0); // Magnitude should be positive
-    }
-
-    #[test]
-    fn test_vector_magnitude_fractional() {
-        let v = Vector { data: Array1::from_vec(vec![1.0 / SQRT_2, 1.0 / SQRT_2]) };
-        let mag = v.magnitude();
-        assert!((mag - 1.0).abs() < 1e-10); // Should be close to 1 with a small error margin
-    }
-
-    #[test]
-    fn test_vector_normalization() {
-        let v = Vector {
-            data: Array1::from_vec(vec![3.0, 4.0]),
-        };
-        let normalized = v.normalize();
-
-        let expected = Array1::from_vec(vec![0.6, 0.8]);
-        assert_eq!(normalized.data, expected);
-    }
-
-    #[test]
-    fn test_matrix_transpose() {
-        let a = Matrix {
-            data: Array2::from_shape_vec((2, 3), vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0]).unwrap(),
-        };
-        let transposed = a.transpose();
-
-        let expected = Array2::from_shape_vec((3, 2), vec![1.0, 4.0, 2.0, 5.0, 3.0, 6.0]).unwrap();
-        assert_eq!(transposed.data, expected);
-    }
-
-    #[test]
-    fn test_matrix_determinant() {
-        let a = Matrix {
-            data: Array2::from_shape_vec((2, 2), vec![1.0, 2.0, 3.0, 4.0]).unwrap(),
-        };
-        let determinant = a.determinant().unwrap();
-
-        let expected = -2.0;
-        assert_eq!(determinant, expected);
-    }
-
-    #[test]
-    fn test_matrix_determinant_non_square() {
-        let a = Matrix {
-            data: Array2::from_shape_vec((2, 3), vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0]).unwrap(),
-        };
-        let determinant = a.determinant();
-
-        assert_eq!(determinant, None);
+    pub fn l2_norm(&self) -> f64 {
+        self.data.par_iter().map(|&x| x * x).sum::<f64>().sqrt()
     }
 }
---
+++ tests/tests.rs
@@ -0,0 +1,275 @@
+use lin_alg_crate::Matrix;
+use lin_alg_crate::Vector;
+use ndarray::{Array1, Array2};
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::f64::consts::SQRT_2;
+
+    fn assert_matrix_eq(a: &Array2<f64>, b: &Array2<f64>, tol: f64) {
+        assert!(a.iter().zip(b.iter()).all(|(&x, &y)| (x - y).abs() < tol));
+    }
+    #[test]
+    fn test_matrix_addition() {
+        let a = Matrix {
+            data: Array2::from_shape_vec((2, 2), vec![1.0, 2.0, 3.0, 4.0]).unwrap(),
+        };
+        let b = Matrix {
+            data: Array2::from_shape_vec((2, 2), vec![4.0, 3.0, 2.0, 1.0]).unwrap(),
+        };
+        let result = a.add(&b).unwrap();
+
+        let expected = Array2::from_shape_vec((2, 2), vec![5.0, 5.0, 5.0, 5.0]).unwrap();
+        assert_eq!(result.data, expected);
+    }
+
+    #[test]
+    #[should_panic(expected = "Matrices must be of the same dimensions")]
+    fn test_matrix_addition_panic() {
+        let a = Matrix {
+            data: Array2::from_shape_vec((2, 2), vec![1.0, 2.0, 3.0, 4.0]).unwrap(),
+        };
+        let b = Matrix {
+            data: Array2::from_shape_vec((3, 2), vec![4.0, 3.0, 2.0, 1.0, 0.0, -1.0]).unwrap(),
+        };
+        a.add(&b);
+    }
+
+    #[test]
+    fn test_matrix_multiplication() {
+        let a = Matrix {
+            data: Array2::from_shape_vec((2, 2), vec![1.0, 2.0, 3.0, 4.0]).unwrap(),
+        };
+        let b = Matrix {
+            data: Array2::from_shape_vec((2, 2), vec![2.0, 0.0, 1.0, 2.0]).unwrap(),
+        };
+        let result = a.multiply(&b).unwrap();
+
+        let expected = Array2::from_shape_vec((2, 2), vec![4.0, 4.0, 10.0, 8.0]).unwrap();
+        assert_eq!(result.data, expected);
+    }
+
+    #[test]
+    #[should_panic(expected = "Inner matrix dimensions must match for multiplication")]
+    fn test_matrix_multiplication_panic() {
+        let a = Matrix {
+            data: Array2::from_shape_vec((2, 3), vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0]).unwrap(),
+        };
+        let b = Matrix {
+            data: Array2::from_shape_vec((2, 2), vec![7.0, 8.0, 9.0, 10.0]).unwrap(),
+        };
+        a.multiply(&b);
+    }
+
+    #[test]
+    fn test_vector_addition() {
+        let a = Vector {
+            data: Array1::from_vec(vec![1.0, 2.0, 3.0]),
+        };
+        let b = Vector {
+            data: Array1::from_vec(vec![4.0, 5.0, 6.0]),
+        };
+        let result = a.add(&b);
+
+        let expected = Array1::from_vec(vec![5.0, 7.0, 9.0]);
+        assert_eq!(result.data, expected);
+    }
+
+    #[test]
+    fn test_vector_dot_product() {
+        let a = Vector {
+            data: Array1::from_vec(vec![1.0, 2.0, 3.0]),
+        };
+        let b = Vector {
+            data: Array1::from_vec(vec![4.0, 5.0, 6.0]),
+        };
+        let result = a.dot(&b).unwrap();
+
+        let expected = 32.0; // 1*4 + 2*5 + 3*6
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    #[should_panic(expected = "Vectors must be of the same length")]
+    fn test_vector_dot_product_panic() {
+        let a = Vector {
+            data: Array1::from_vec(vec![1.0, 2.0, 3.0]),
+        };
+        let b = Vector {
+            data: Array1::from_vec(vec![4.0, 5.0]),
+        };
+        a.dot(&b);
+    }
+    #[test]
+    fn test_vector_magnitude_zero() {
+        let v = Vector {
+            data: Array1::from_vec(vec![0.0, 0.0, 0.0]),
+        };
+        let mag = v.magnitude();
+        assert_eq!(mag, 0.0);
+    }
+
+    #[test]
+    fn test_vector_magnitude_basic() {
+        let v = Vector {
+            data: Array1::from_vec(vec![3.0, 4.0]),
+        };
+        let mag = v.magnitude();
+        assert_eq!(mag, 5.0); // 3-4-5 right triangle
+    }
+
+    #[test]
+    fn test_vector_magnitude_negative_values() {
+        let v = Vector {
+            data: Array1::from_vec(vec![-3.0, -4.0]),
+        };
+        let mag = v.magnitude();
+        assert_eq!(mag, 5.0); // Magnitude should be positive
+    }
+
+    #[test]
+    fn test_vector_magnitude_fractional() {
+        let v = Vector {
+            data: Array1::from_vec(vec![1.0 / SQRT_2, 1.0 / SQRT_2]),
+        };
+        let mag = v.magnitude();
+        assert!((mag - 1.0).abs() < 1e-10); // Should be close to 1 with a small error margin
+    }
+
+    #[test]
+    fn test_vector_normalization() {
+        let v = Vector {
+            data: Array1::from_vec(vec![3.0, 4.0]),
+        };
+        let normalized = v.normalize();
+
+        let expected = Array1::from_vec(vec![0.6, 0.8]);
+        assert_eq!(normalized.data, expected);
+    }
+
+    #[test]
+    fn test_matrix_transpose() {
+        let a = Matrix {
+            data: Array2::from_shape_vec((2, 3), vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0]).unwrap(),
+        };
+        let transposed = a.transpose();
+
+        let expected = Array2::from_shape_vec((3, 2), vec![1.0, 4.0, 2.0, 5.0, 3.0, 6.0]).unwrap();
+        assert_eq!(transposed.data, expected);
+    }
+
+    #[test]
+    fn test_matrix_determinant() {
+        let a = Matrix {
+            data: Array2::from_shape_vec((2, 2), vec![1.0, 2.0, 3.0, 4.0]).unwrap(),
+        };
+        let determinant = a.determinant().unwrap();
+
+        let expected = -2.0;
+        assert_eq!(determinant, expected);
+    }
+
+    #[test]
+    fn test_matrix_determinant_non_square() {
+        let a = Matrix {
+            data: Array2::from_shape_vec((2, 3), vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0]).unwrap(),
+        };
+        let determinant = a.determinant();
+
+        assert_eq!(determinant, None);
+    }
+    #[test]
+    fn test_matrix_inverse() {
+        let a = Matrix {
+            data: Array2::from_shape_vec((2, 2), vec![4.0, 7.0, 2.0, 6.0]).unwrap(),
+        };
+        let inverse = a.inverse().unwrap();
+
+        let expected = Array2::from_shape_vec((2, 2), vec![0.6, -0.7, -0.2, 0.4]).unwrap();
+        assert_matrix_eq(&inverse.data, &expected, 1e-10);
+    }
+
+    #[test]
+    #[should_panic(expected = "Matrix is not invertible")]
+    fn test_matrix_inverse_non_invertible() {
+        let a = Matrix {
+            data: Array2::from_shape_vec((2, 2), vec![1.0, 2.0, 2.0, 4.0]).unwrap(),
+        };
+        a.inverse().unwrap();
+    }
+    #[test]
+    fn test_matrix_lu_decomposition() {
+        let a = Matrix {
+            data: Array2::from_shape_vec((3, 3), vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]).unwrap(),
+        };
+        let (l, u) = a.lu_decomposition().unwrap();
+
+        let l_expected = Array2::from_shape_vec((3, 3), vec![1.0, 0.0, 0.0, 4.0, -3.0, 0.0, 7.0, -6.0, 0.0]).unwrap();
+        let u_expected = Array2::from_shape_vec((3, 3), vec![1.0, 2.0, 3.0, 0.0, -3.0, -6.0, 0.0, 0.0, 0.0]).unwrap();
+
+        assert_matrix_eq(&l.data, &l_expected, 1e-10);
+        assert_matrix_eq(&u.data, &u_expected, 1e-10);
+    }
+    #[test]
+    fn test_matrix_l1_norm() {
+        let a = Matrix {
+            data: Array2::from_shape_vec((2, 2), vec![1.0, -2.0, 3.0, -4.0]).unwrap(),
+        };
+        let norm = a.l1_norm();
+
+        let expected = 10.0; // |1| + |-2| + |3| + |-4|
+        assert_eq!(norm, expected);
+    }
+
+    #[test]
+    fn test_matrix_l2_norm() {
+        let a = Matrix {
+            data: Array2::from_shape_vec((2, 2), vec![1.0, 2.0, 3.0, 4.0]).unwrap(),
+        };
+        let norm = a.l2_norm();
+
+        let expected = 5.477225575051661; // sqrt(1^2 + 2^2 + 3^2 + 4^2)
+        assert_eq!(norm, expected);
+    }
+
+    #[test]
+    fn test_matrix_infinity_norm() {
+        let a = Matrix {
+            data: Array2::from_shape_vec((2, 2), vec![1.0, -2.0, -3.0, 4.0]).unwrap(),
+        };
+        let norm = a.infinity_norm();
+
+        let expected = 7.0; // max(|1 - 2|, |-3 + 4|)
+        assert_eq!(norm, expected);
+    }
+    #[test]
+    fn test_matrix_trace() {
+        let a = Matrix {
+            data: Array2::from_shape_vec((2, 2), vec![1.0, 2.0, 3.0, 4.0]).unwrap(),
+        };
+        let trace = a.trace();
+
+        let expected = 5.0; // 1 + 4
+        assert_eq!(trace, expected);
+    }
+    #[test]
+    fn test_vector_l1_norm() {
+        let v = Vector {
+            data: Array1::from_vec(vec![1.0, -2.0, 3.0, -4.0]),
+        };
+        let norm = v.l1_norm();
+
+        let expected = 10.0; // |1| + |-2| + |3| + |-4|
+        assert_eq!(norm, expected);
+    }
+    #[test]
+    fn test_vector_l2_norm() {
+        let v = Vector {
+            data: Array1::from_vec(vec![1.0, 2.0, 3.0, 4.0]),
+        };
+        let norm = v.l2_norm();
+
+        let expected = 5.477225575051661; // sqrt(1^2 + 2^2 + 3^2 + 4^2)
+        assert!((norm - expected).abs() < 1e-10);
+    }
+}
